2d9834d15bc048bac80544f9b5664963fe2f33e0
Alejandro_PC <alejoveintimilla@gmail.com>
2024-10-09 11:50:47 -0500

    csrf_trusted_origins

--- backend/SintelPlatform/settings.py
+++ backend/SintelPlatform/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 DOCKER = True
 
 ALLOWED_HOSTS = ["*"]
-CSRF_TRUSTED_ORIGINS = ['*']
+CSRF_TRUSTED_ORIGINS = ["https://sintel.alejandroveintimilla.com/", "http://sintel.alejandroveintimilla.com/"]
 
 # Application definition
 
